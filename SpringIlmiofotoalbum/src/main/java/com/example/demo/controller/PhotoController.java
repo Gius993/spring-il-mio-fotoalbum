6d7ae8480a6c4eb5abce1563793774d5c543ea9c
Gius993 <giuseppevidili9@gmail.com>
2023-03-02 12:44:33 +0100

    add edit

--- SpringIlmiofotoalbum/src/main/java/com/example/demo/controller/PhotoController.java
+++ SpringIlmiofotoalbum/src/main/java/com/example/demo/controller/PhotoController.java
@@ -5,12 +5,15 @@ package com.example.demo.controller;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -59,6 +62,51 @@ public class PhotoController {
 		}
 		
 		photorepo.save(photo);
+		return "redirect:/photos";
+	}
+	@GetMapping("/show/{id}")
+	public String show(
+			@PathVariable("id") int id,
+			Model model) {
+		
+		Optional<Photo> optPhoto = photorepo.findById(id);
+		
+		Photo photo = optPhoto.get();
+		
+		model.addAttribute("photo", photo);
+		
+		return "photo/photo-show";
+	}
+
+
+	@GetMapping("/edit/{id}")
+	public String edit(
+			@PathVariable("id") int id,
+			Model model
+		) {
+		
+		Photo photo = photorepo.findById(id).get();
+		
+		model.addAttribute("photo", photo);
+		
+		return "photo/photo-edit";
+	}
+	
+	@PostMapping("/edit")
+	public String update(
+			@Valid @ModelAttribute Photo formPhoto,
+			BindingResult bindingResult,
+			RedirectAttributes redirectAttributes,
+			Model model
+		) {
+		
+		if (bindingResult.hasErrors()) {
+			
+			redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+			return "redirect:/photos/edit/" + formPhoto.getId();
+		}
+		photorepo.save(formPhoto);
+		
 		return "redirect:/photos";
 	}
 }
