eb8d3f44b638fdf6f539dc3fff069e08d37187ac
Gius993 <giuseppevidili9@gmail.com>
2023-03-02 11:05:23 +0100

    correction id and add html

--- SpringIlmiofotoalbum/src/main/java/com/example/demo/model/Photo.java
+++ SpringIlmiofotoalbum/src/main/java/com/example/demo/model/Photo.java
@@ -2,10 +2,13 @@ package com.example.demo.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Photo {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
 	private int id;
 	
