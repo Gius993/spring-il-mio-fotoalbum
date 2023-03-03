fddb3a9e4689e07f2e25633a64ba33c70ae801f3
Gius993 <giuseppevidili9@gmail.com>
2023-03-03 10:25:45 +0100

    filter name

--- fotoalbumfront/index.js
+++ fotoalbumfront/index.js
@@ -1,14 +1,20 @@
-function photoList(){
-	axios.get("http://localhost:8080/api/photos").then((list) =>{
-		console.log(list);
-		document.getElementById("photo_list").innerHTML = '';
-		list.data.forEach(photo => {
-		document.getElementById("photo_list").innerHTML += `<li>${photo.title}</li> `;
-		});
-		
-	}).catch((list) =>{
-	console.error("non si puo eseguire la pagina riprova dopo" , list);
-	alert('Errore durante la connessione');
-	}); 
+function photoList(keyword){
+    axios.get("http://localhost:8080/api/photos").then((list) =>{
+        console.log(list);
+        document.getElementById("photo_list").innerHTML = '';
+        list.data
+            .filter(photo => photo.title.includes(keyword)) // filtra le foto in base alla parola chiave
+            .forEach(photo => {
+                document.getElementById("photo_list").innerHTML += `<li>${photo.title}</li> `;
+            });
+        
+    }).catch((list) =>{
+        console.error("non si puo eseguire la pagina riprova dopo" , list);
+        alert('Errore durante la connessione');
+    }); 
 }
-photoList();
+function searchPhotos(){
+	const keyword = document.getElementById("keyword").value;
+	photoList(keyword);
+  }
+  photoList(keyword);
